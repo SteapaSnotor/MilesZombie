cc6fd82a9713965b89fad9174831a098c4f2ef8c
SteapaSnotor <santoslucas347@gmail.com>
2020-08-14 16:02:12 -0300

    Graveyard level

---
+++ resources/tiles/graveyard/Graveyard2_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="Graveyard2_tileset" tilewidth="64" tileheight="64" tilecount="15" columns="5">
+ <image source="../../../sprites/tiles/graveyard/tilemap.png" width="320" height="200"/>
+</tileset>
---
+++ resources/tiles/graveyard/graveyard_tileset.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="graveyard_tileset" tilewidth="64" tileheight="64" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="../../../sprites/tiles/graveyard/grass_0.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="../../../sprites/tiles/graveyard/grass_1.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="../../../sprites/tiles/graveyard/grass_2.png"/>
+ </tile>
+</tileset>
