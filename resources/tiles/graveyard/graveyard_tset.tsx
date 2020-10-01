7908286c568fe65635b4cfd2be77d5ed10c05c79
SteapaSnotor <santoslucas347@gmail.com>
2020-10-01 19:51:30 -0300

    New cursor graphics

---
+++ resources/tiles/graveyard/graveyard_tset.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="graveyard_tset" tilewidth="64" tileheight="64" tilecount="225" columns="25">
+ <editorsettings>
+  <export target="graveyard_tset.tres" format="Godot"/>
+ </editorsettings>
+ <image source="../../../sprites/tiles/graveyard/tilemap.png" width="1640" height="600"/>
+ <tile id="26">
+  <properties>
+   <property name="projectRoot" value="C:/Users/Lucas/Desktop/Godot Projects/Projects/Miles The Zombie/"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ resources/tiles/graveyard_old_tiled/Graveyard2_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="Graveyard2_tileset" tilewidth="64" tileheight="64" tilecount="60" columns="10">
+ <image source="../../../sprites/tiles/graveyard/tilemap.png" width="640" height="400"/>
+</tileset>
